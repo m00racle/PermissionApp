09e4ac0a1b9fc5aaf57cab44e9107b3bc68392bc
mooracle <yanuar.prakosa@gmail.com>
2019-01-15 21:34:44 +0700

    Explaining Ourselves

--- app/src/main/java/com/mooracle/permissionapp/MainActivity.java
+++ app/src/main/java/com/mooracle/permissionapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mooracle.permissionapp;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -10,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,15 +46,41 @@ public class MainActivity extends AppCompatActivity {
         // start the new activity using the intent: (promptly will invoke error due lack of permission)
         // Fix add permission check
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            //  Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},
-                    PERMISSIONS_REQUEST_CODE);
+            // set a message using the latest Activity compat shouldshowRequestPermissionRationale method:
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)){
+                // set message
+                new AlertDialog.Builder(this).setTitle("Call Permission Needed")
+                        .setMessage("The app needs your permission to call, please grant it")
+                        .setPositiveButton("Yep", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // recall the request permission:
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                        Manifest.permission.CALL_PHONE
+                                }, PERMISSIONS_REQUEST_CODE);
+                            }
+                        })
+                        .setNegativeButton("Nope", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this,
+                                        "Sorry, the app cannot operate with no call permission :(",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }).show();
+            }// set else scenario thus lock the app if finally the user click deny and not to ask again:
+            else {
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},
+                        PERMISSIONS_REQUEST_CODE);
+            }
+
+
         }else {startActivity(intent);}
 
     }
@@ -62,8 +91,12 @@ public class MainActivity extends AppCompatActivity {
         // check if the requestCode is not null and same as PERMISSION_REQUEST_CODE
         if (requestCode == PERMISSIONS_REQUEST_CODE){
             if (grantResults.length > 0
-            && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED){ //permission granted
                 makeCall();
+            }else { //permission denied
+                Toast.makeText(MainActivity.this,
+                        "Sorry, the app cannot operate with no call permission :(",
+                        Toast.LENGTH_LONG).show();
             }
         }
     }
